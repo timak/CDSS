e1e657a0277c5a4563d5e40fb10ac1191324b3cd
Branislav Lukac <branoluky@gmail.com>
2013-05-21 10:46:41 +0200

    fixed error

--- CDSS_TEXTMINING/src/stuba/fei/tp/cdss/service/TextMiningDataServiceImpl.java
+++ CDSS_TEXTMINING/src/stuba/fei/tp/cdss/service/TextMiningDataServiceImpl.java
@@ -1,14 +1,13 @@
 package stuba.fei.tp.cdss.service;
 
 import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 import stuba.fei.tp.cdss.dto.Symptom;
 import stuba.fei.tp.cdss.parser.XMLSymptomFilter;
 import stuba.fei.tp.cdss.parser.XMLgetSymptomName;
-import stuba.fei.tp.cdss.utils.XMLFileHelper;
+
+import com.sun.org.apache.xml.internal.security.utils.Base64;
 
 public class TextMiningDataServiceImpl implements TextMiningDataService {
 
@@ -27,17 +26,13 @@ public class TextMiningDataServiceImpl implements TextMiningDataService {
 
 	@Override
 	public String getMedicalRecords(String disease, String[] requiredSymptoms) { 
-<<<<<<< HEAD
-		
-		String recordsXML = XMLFileHelper.convertXMLFileToString("/records.xml");
-=======
+
 		XMLSymptomFilter xmlSymptomFilter = new XMLSymptomFilter(requiredSymptoms);
 		InputStream fileInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("/diabetes.xml");
         xmlSymptomFilter.load(fileInputStream);
 
         String recordsXML = xmlSymptomFilter.getXML();
         System.out.println("Debug: Records - " + recordsXML);
->>>>>>> 03015d3789031415c9c3d341df91253fbb8621f6
 
         // encode xml with base 64 algorithm
         String encodedXml = Base64.encode(recordsXML.getBytes());
